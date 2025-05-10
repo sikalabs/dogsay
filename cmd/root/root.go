2009c018bacc8beaf350fb43e620fdb7b44b681d
Ondrej Sika <ondrej@ondrejsika.com>
2025-05-10 15:38:00 +0200

    feat(cmd/root): Make root command dogsay

--- cmd/root/root.go
+++ cmd/root/root.go
@@ -1,11 +1,16 @@
 package root
 
 import (
+	"github.com/sikalabs/dogsay/pkg/dogsay"
 	"github.com/sikalabs/dogsay/version"
 	"github.com/spf13/cobra"
 )
 
 var Cmd = &cobra.Command{
-	Use:   "dogsay",
+	Use:   "dogsay <text>",
 	Short: "dogsay, " + version.Version,
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		dogsay.PrintDogSay(args[0])
+	},
 }
